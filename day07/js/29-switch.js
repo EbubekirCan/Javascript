1117b76d80fc632369ea5ce1d4addec0929d3122
Ebubekir <ebubekirc442@gmail.com>
2023-08-17 11:34:42 +0300

    17.08.23

--- day07/js/29-switch.js
+++ day07/js/29-switch.js
@@ -7,45 +7,62 @@ function setPrice() {
     let result = +priceEl.value;
     let discount;
 
-    // switch (true) {
-    //     case result >= 1500:
-    //         discount = 15;
-    //         break;
-    //     case result >= 1000:
-    //         discount = 13;
-    //         break;
-    //     case result >= 700:
-    //         discount = 10;
-    //         break;
-    //     case result >= 500:
-    //         discount = 7;
-    //         break;
-    //     case result >= 300:
-    //         discount = 5;
-    //         break;
-    //     default:
-    //         discount = 2;
-    // }
-
-    switch (result) {
-        case 1500:
+    // BIRINCI SENARYO
+    switch (true) {
+        case result >= 1500:
             discount = 15;
             break;
-        case 1000:
+        case result >= 1000:
             discount = 13;
             break;
-        case 700:
+        case result >= 700:
             discount = 10;
             break;
-        case 500:
+        case result >= 500:
             discount = 7;
             break;
-        case 300:
+        case result >= 300:
             discount = 5;
             break;
         default:
             discount = 2;
     }
+
+    // if (result >= 1500) {
+    //     discount = 15;
+    // } else if (result >= 1000) {
+    //     discount = 13;
+    // } else if (result >= 700) {
+    //     discount = 10;
+    // } else if (result >= 500) {
+    //     discount = 7;
+    // } else if (result >= 300) {
+    //     discount = 5;
+    // } else {
+    //     discount = 2;
+    // }
+
+    // IKINCI SENARYO
+
+    // switch (result) {
+    //     case 1500:
+    //         discount = 15;
+    //         break;
+    //     case 1000:
+    //         discount = 13;
+    //         break;
+    //     case 700:
+    //         discount = 10;
+    //         break;
+    //     case 500:
+    //         discount = 7;
+    //         break;
+    //     case 300:
+    //         discount = 5;
+    //         break;
+    //     default:
+    //         discount = 2;
+    // }
 
     // if (result === 1500) {
     //     discount = 15;
