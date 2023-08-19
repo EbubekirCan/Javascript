2291609c0bbf60294772b622579b581cfe33b526
Ebubekir <ebubekirc442@gmail.com>
2023-08-19 21:34:23 +0300

    19.08.23

--- day10/js/41-functions.js
+++ day10/js/41-functions.js
@@ -1,14 +1,33 @@
-function getage(){
-    console.log("a")
-    
-}function getage(){
-    console.log("a")
+// DOM ELEMENTS
+const inputEl = document.querySelector("#birthdate");
 
-}function getage(){
-    console.log("a")
-
+function getAge() {
+    console.log(`I'm ${2023 - 1995} years old`);
 }
 
-getage();
+getAge();
 
-Date.now
+const thisYear = 2023;
+
+
+function getAge1() {
+    const birthYear = +inputEl.value;
+    console.log(`I'm ${thisYear - birthYear} years old`)
+};
+
+function getAge2(year, birth, name) {
+    console.log(`I'm ${year - birth} years old`)
+    console.log(`My name is ${name ?? "Anonymous"}`);
+};
+
+getAge2(2023, 1995, "John");
+
+getAge2(2020, 1990, "Jane");
+
+getAge2(2023, 1995);
+
+function topla(a, b) {
+    console.log(a + b);
+};
+
+topla(5, 10);
