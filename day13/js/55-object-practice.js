3281817ee815e11d735d59faff0821ae8d23c577
Ebubekir <ebubekirc442@gmail.com>
2023-08-23 22:52:52 +0300

    23.08.23

---
+++ day13/js/55-object-practice.js
@@ -0,0 +1,12 @@
+const car = {
+    brand: "Audi",
+    model: "A4",
+    color: "red",
+    year: 2019,
+    doors: 4,
+    seats: 5,
+    engine: "2.0 TDI",
+    fuel: "diesel",
+    transmission: "Automatic",
+    price: 20000,
+}
